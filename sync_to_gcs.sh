0d1ad0715cf912ce9539f5458d72fc07d91f1ff6
SachinKonan <sachinkonan480@gmail.com>
2026-02-05 23:06:54 -0500

    added

--- sync_to_gcs.sh
+++ sync_to_gcs.sh
@@ -1,5 +1,5 @@
 gcloud alpha storage rsync -r \
   --gzip-in-flight-all \
-  -x '^(?!dataset_info\.json|iclr_|images/).*' \
+  -x '^(?!dataset_info\.json|iclr_|images/|2017_|2020_).*' \
   data \
   gs://autoreviewer-data/autoreviewer_data
